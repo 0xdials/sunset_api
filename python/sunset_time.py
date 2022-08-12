ad63c923e473686c0eee1b4451f3e1f51dadfb80
dials <tastywaves56k@gmail.com>
2022-08-11 21:31:53 -0700

    python version

--- python/sunset_time.py
+++ python/sunset_time.py
@@ -1,17 +1,65 @@
 #!/user/bin/python
 import requests
-from datetime import datetime, tzinfo
-import pytz
+import json
+from datetime import datetime
+from dateutil import tz
 
+# setup local timezone information, preset is "auto-detect"
+from_zone = tz.tzutc()
+to_zone = tz.tzlocal()
+
+# uncomment to set manually timezones
+# from_zone = tz.gettz('UTC')
+# to_zone = tz.gettz('America/New_York')
+
+#current_hour = int(datetime.now().strftime("%H"))
+current_hour = 10
+
+# input lat/long info for api call
 lat = "47.6062"
 long = "-122.3321"
-
 url = f"https://api.sunrise-sunset.org/json?lat={lat}&lng={long}&formatted=0"
 
+
+def main(sunrise, sunset, current_time):
+	if current_time >= sunset:
+		return(" Evening")
+	elif current_time >= sunrise & current_time < 12:
+		return(" Morning")
+	elif current_time >= 12:
+		return(" Afternoon")
+	else:
+		return(" Evening")
+
 def api_call():
+	response = requests.get(url, verify=False).text
+	response_json = json.loads(response)
+	return(response_json)
+	
+	
+def sunrise(results):
+	sunrise = results['results']['sunrise']
+	sunrise_parse = datetime.strptime(sunrise, "%Y-%m-%d" + "T" + "%H:%M:%S" + "+00:00")
+	utc = sunrise_parse.replace(tzinfo=from_zone)
+	adjusted_sunrise = utc.astimezone(to_zone)
+	return(int(adjusted_sunrise.hour))
 
-	response = requests.get(url, verify=False)
-	print(response.json('sunrise'))
+
+def sunset(results):
+	sunset = results['results']['sunset']
+	sunset_parse = datetime.strptime(sunset, "%Y-%m-%d" + "T" + "%H:%M:%S" + "+00:00")
+	utc = sunset_parse.replace(tzinfo=from_zone)
+	adjusted_sunset = utc.astimezone(to_zone)
+	return(int(adjusted_sunset.hour))
+	
 
 
-api_call()
+results = api_call()
+sunrise_time = sunrise(results)
+sunset_time	= sunset(results)
+
+sunset(results)
+print(sunrise_time, sunset_time, current_hour)
+print(main(sunrise_time, sunset_time, current_hour))
+
+
